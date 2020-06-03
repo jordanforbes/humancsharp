3c428f03b9ed1e15fadc046be4a2c667e3fdaaab
jordanforbes <jordforb@gmail.com>
2020-06-02 21:49:38 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace humanproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
